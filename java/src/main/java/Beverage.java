afa05e5448324072a4073a68120d418f909f7ed6
jomarlub17 <161365974+jomarlub17@users.noreply.github.com>
2026-04-22 09:22:39 -0400

    Created  abstract Beverage and Main

---
+++ java/src/main/java/Beverage.java
@@ -0,0 +1,21 @@
+/**
+ * @Author Jomar Lubin
+ * @Author Jake "Write Your last name here Jake"
+ *
+ * After reading and understanding the assignment, I deemed the
+ * previous "HotSetFactory.java" and "DriverClass.java" unnecessary
+ *
+ * The instruction are clear on the names of the classes we need.
+ *
+ * Beverage being the first one which needs to either be an Interface or Abstract
+ *
+ * I chose to make it abstract because we will be implementing the "Abstract Factory Design"
+ */
+public abstract class Beverage {
+
+    // here we should create the options fields like  hot, iced, sugar, milk etc
+}
+
+
+
+
---
+++ java/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * A placeHolder method to allow the creation of the abstract method Beverage
+ *
+ * we can(must) later change the name to something more appropriate
+ *
+ * also This intellij Project is supposed to be named "RamsBrewOrderSystem"
+ *
+ * But I doubt it will be a problem if we live it as it is.
+ */
+public class Main {
+    static void main() {
+
+    }
+}
